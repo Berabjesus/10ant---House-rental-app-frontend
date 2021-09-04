ad610b40a0ef569f56c2fa7e36e76a1616d43705
berabjesus <berabjesus@gmail.com>
2021-09-04 12:11:54 -0500

    add react router dependancy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router as BrowserRouter   as Router from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
