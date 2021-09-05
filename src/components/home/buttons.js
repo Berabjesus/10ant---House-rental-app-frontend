6af2e06c90ab4089f528f23c8ecdfa11d7f5ddb1
berabjesus <berabjesus@gmail.com>
2021-09-05 13:40:00 -0500

    update landing page theme

--- src/components/home/buttons.js
+++ src/components/home/buttons.js
@@ -2,7 +2,7 @@ import style from './buttons.module.css';
 
 const Buttons = ({text}) => {
   return (
-    <button className={`btn btn-light px-3 py-2 my-2 fw-bold ${style.landing_btns} ${style.glow_button}`}>
+    <button className={`btn btn-light px-3 py-2 my-2 me-3 fw-bold ${style.landing_btns} ${style.glow_button}`}>
       {text}
     </button>
   )
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -6,11 +6,13 @@ const home = () => {
     <section className={`vh_100 pt-xl-5 ps-md-5 ${style.section}`}>
       <figure className="mb-0"></figure>
       <div className='d-flex flex-column px-5 pt-5 col-12 col-md-6'>
-        <h2 className={`display-1 text-center pb-2 font_logo text-black ${style.logo}`}>10Ant</h2>
-        <div className={`font_os text-black fw-bolder ${style.description}`}>
+        <div className="pb-2 mb-2 ">
+          <h2 className={`display-1 p-0 m-0 font_logo text-black border-bottom border-dark w-auto ${style.logo}`}>10Ant</h2>
+        </div>
+        <div className={`font_os text-black fw-bolder pt-2 ${style.description}`}>
           <p >Find affordable deals on apartments and houses for rent. </p>
           <p>Are you looking to rent your house or apartment? <br /> Upload your place here, millions of users visit our website daily.</p>
-          <div className='d-flex flex-wrap justify-content-around pt-md-4 mx-auto'>
+          <div className='d-flex flex-wrap pt-md-4 mx-auto'>
             <Buttons text="Login"/>
             <Buttons text="Sign up"/>
           </div>
