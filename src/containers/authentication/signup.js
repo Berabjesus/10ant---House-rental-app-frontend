7798998893b5412a3cc0c72efb8483a346d49df9
berabjesus <berabjesus@gmail.com>
2021-09-06 00:46:17 -0500

    add menu page setup

--- src/containers/authentication/signup.js
+++ src/containers/authentication/signup.js
@@ -16,23 +16,21 @@ const Signup = () => {
     <>
       <section className={`d-flex justify-content-center pt-3 vh-100 debug_border ${styles.section}`}>
         <div className={`d-flex flex-column flex-md-row ${styles.form_box} ${styles.signup_menu}`}>
-          <a className={`col-12 col-md-6 d-flex p-2 p-md-3 debug_border col-12 col-md-6 d-flex  debug_border ${styles.signup_menu_container}`}>
-            <div class="position-relative text-black font_rw overflow-hidden z_index_max">
-              <figure class={`z_index_min ${styles.menu} ${styles.menu_left}`} alt="tenant_image"></figure>
-              <div class="z_index_max">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
+          <a href="#" className={`col-12 col-md-6 d-flex p-2 p-md-3 col-12 col-md-6 d-flex ${styles.signup_menu_anchor}`}>
+            <div class={`position-relative text-black font_rw overflow-hidden border border-1 border-dark rounded-3 z_index_max w-100 hover_custom`}>
+              <figure class={`z_index_min ${styles.menu_items} ${styles.menu_left}`} alt="tenant_image"></figure>
+              <div class={` pt-5 px-3 h-100 z_index_max ${styles.menu_text_container}`}>
+                <h3 class="card-title font_rw fw-bold">Are you a tenant looking to rent a new house or apartment?</h3>
+                <h3 className='text-center mt-4'>Click Here</h3>
               </div>
             </div>
           </a>
-          <a className={`col-12 col-md-6 d-flex p-2 p-md-3 debug_border col-12 col-md-6 d-flex  debug_border ${styles.signup_menu_container}`}>
-            <div class="position-relative text-black font_rw overflow-hidden z_index_max">
-              <figure class={`z_index_min ${styles.menu} ${styles.menu_right}`} alt="tenant_image"></figure>
-              <div class="z_index_max">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Last updated 3 mins ago</p>
+          <a href="#" className={`col-12 col-md-6 d-flex p-2 p-md-3 col-12 col-md-6 d-flex ${styles.signup_menu_anchor}`}>
+            <div class={`position-relative text-black font_rw overflow-hidden border border-1 border-dark rounded-3 z_index_max w-100 hover_custom`}>
+              <figure class={`z_index_min ${styles.menu_items} ${styles.menu_right}`} alt="tenant_image"></figure>
+              <div class={` pt-5 px-3 h-100 z_index_max ${styles.menu_text_container}`}>
+                <h3 class="card-title font_rw fw-bold">Are you an owner of a house looking to find new tenants?</h3>
+                <h3 className='text-center mt-4'>Click Here</h3>
               </div>
             </div>
           </a>
