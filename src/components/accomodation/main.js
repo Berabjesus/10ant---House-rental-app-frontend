b2490916e511fcd4557f44c71a56824f2a510775
berabjesus <berabjesus@gmail.com>
2021-09-07 20:09:38 -0500

    add search box and filter

--- src/components/accomodation/main.js
+++ src/components/accomodation/main.js
@@ -1,16 +1,22 @@
+import styles from './accomodation.module.css'
+
 const Main = () => {
   return (
-    <section className="col-12 col-md-7 col-lg-8 col-xl-9 px-5 debug_border">
-      <div class="input-group mb-3">
-        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-        <input type="text" class="form-control" aria-label="Text input with dropdown button" />
+    <section className="col-12 col-md-7 col-lg-8 col-xl-9 px-5 d-flex debug_border">
+      <div className ="col-12 d-flex">
+        <div class="d-flex col-7">
+          <input type="text" class=" form-control" aria-label="Text input" placeholder="Search for anything"/>
+        </div>
+        <div class="dropdown ms-3">
+          <a class="btn btn-dark dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            Sort by
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Price</a></li>
+            <li><a class="dropdown-item" href="#">Date</a></li>
+          </ul>
+        </div>
       </div>
-
     </section>
   )
 }
