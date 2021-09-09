1ad858f22c10bfb3099d07b132cf34278b7ea746
berabjesus <berabjesus@gmail.com>
2021-09-08 21:32:52 -0500

    add user rating for items

--- src/components/accomodation/display.js
+++ src/components/accomodation/display.js
@@ -9,7 +9,7 @@ const Display = () => {
     type: 'apartment',
     address: 'Iowa',
     price: 400,
-    rating: 3,
+    rating: 0,
     image: test,
     description : 'skldflk jkldj klsdklf jsdjkf kljsdlkfj ',
     uploadedBy: 'Berekt'
@@ -93,23 +93,44 @@ const Display = () => {
                 <div className={`p-2 font_rw d-flex flex-column text-start w-100 h-100 ${styles.img_description}`}>
                   <h4 className="border-bottom border-dark border-2">Apartment</h4>
                   <small className="text-dark">Posted by <ins className=" text-decoration-none fw-bold">Jhon</ins></small>
-                  <h3 className="mt-auto fw-bold  p-2 w-50 border-bottom border-dark text-center">400$</h3>
+                  <h4 className="mt-auto fw-bold p-2 align-self-start border border-dark bg-light bg-opacity-50 text-left">400$</h4>
                 </div>
                 <div className={`w-100 h-100 font_rw ${styles.image_container}`}>
                   <img src={test} className={`h-100 w-100 ${styles.image}`} alt="" />
                 </div>
               </button>
-              <div className="d-flex justify-content-between align-items-center py-2 border bg-light">
-                <FontAwesomeIcon
-                  icon={faStarSolid}
-                  style={{ color: 'Black', fontSize: '15px' }}>
-                </FontAwesomeIcon>
-                <button className="m-0 p-1 d-flex flex-row border border-dark">
+              <div className="d-flex justify-content-between align-items-center p-2 border bg-light">
+                {
+                  (()=> {
+                    const stars = []
+                    if (item.rating > 5) {
+                      return;
+                    }
+                    for (let i = 0; i < item.rating; i++) {
+                        stars.push (
+                          <FontAwesomeIcon
+                            icon={faStarSolid}
+                            style={{ color: 'Black', fontSize: '15px' }}>
+                          </FontAwesomeIcon>
+                        )
+                    }
+                    for (let i = 0; i < 5 - item.rating; i++) {
+                      stars.push (
+                        <FontAwesomeIcon
+                          icon={faStar}
+                          style={{ color: 'gray', fontSize: '15px' }}>
+                        </FontAwesomeIcon>
+                      )
+                    }
+                    return stars;
+                  })()
+                }
+                <button className="m-0 p-1 d-flex flex-row border border-dark btn btn-light">
                   <FontAwesomeIcon
                     icon={faPlusSquare}
-                    style={{ color: 'Black', fontSize: '25px' }}>
+                    style={{ color: 'Black', fontSize: '20px' }}>
                   </FontAwesomeIcon>
-                  <strong className="ms-2">Add to Wishlist</strong>
+                  <strong className="ms-2 h6 mb-0">Add to Wishlist</strong>
                 </button>
               </div>
           </div>
